61e678a216a3c8ea109a5f57564ba50be27d8d46
Eduardo <esantamaria32@yahoo.com>
2021-03-13 18:51:13 -0600

    Guia 6

---
+++ Guia6/Gua_6_Ejercicio_1/app/src/main/java/com/example/gua_6_ejercicio_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gua_6_ejercicio_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+    }
+}
